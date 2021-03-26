786b0c46055d115169cf233ec1ea551c226156cd
thraddash <theartist@gmail.com>
2021-03-25 18:06:47 -0700

    JSX reference js variables

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click on Me!';
+}
+
 // Create a react component
 const App = () => {
-    return (
-      <div>
-        <label className="label" for="name">
-          Enter name:
-        </label>
-        <input id="name" type="text" />
-        <button style={{ backgroundColor: 'blue', color: 'white' }}>
-          Submit
-        </button>
-      </div>
-    );
+
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
